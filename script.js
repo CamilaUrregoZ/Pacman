ef68b24bbb7276dd63f590731b98fa1a6b6fe111
CamilaUrregoZ <camilauz2002@gmail.com>
2025-05-14 19:38:44 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,56 @@
+document.addEventListener('DOMContentLoaded' , () => {
+    const grid = document.querySelector(".grid");
+    const cells = Array.from(grid.querySelectorAll('div'));
+    const width = 10;
+    let pacmanIndex = 11;
+
+    class Ghost{
+
+        constructor(name, startIndex, className, speed = 500){
+            this.name = name;
+            this.currentIndex = startIndex;
+            this.className = className;
+            this.speed = speed;
+            this.timerId = null;
+            this.directions = [-1, 1, -width, width];
+        }
+        
+        draw(){
+            cells[this.currentIndex].classList.add('ghost',this.className);
+           }
+        
+           erase(){
+            cells[this.currentIndex].classList.remove('ghost',this.className);
+           }
+        
+          move(){
+            const moveGhost = () => {
+                const directions = this.directions[Math.floor(Math.random () * this.directions.length )];
+                const nextIndex = this.currentIndex + directions;
+                if (
+                    !cells[nextIndex].classList.contains('Wall') &&
+                    !cells[nextIndex].classList.contains('ghost')
+                ){
+                    this.erase();
+                    this.currentIndex = nextIndex;
+                    this.draw();
+                } else{
+                    moveGhost();
+                }
+
+            };
+
+            this.timerId = setInterval(moveGhost, this.speed);
+          } 
+    
+    }
+
+        const blinky = new Ghost("blinky", 50, 'blue', 350);
+        const pinky = new Ghost("pinky", 67, 'pink', 450);
+        const ghosts = [blinky, pinky];
+
+        ghosts.forEach(ghost =>{
+        ghost.draw();
+        ghost.move();
+         });
+        });
